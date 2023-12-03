95999c423fd12e5c10627966bf8242da73ea1700
e63062kp <konrad.painta@student.manchester.ac.uk>
2023-12-03 20:44:56 +0000

    Further functionality in board tools

--- src/util/BoardTools.java
+++ src/util/BoardTools.java
@@ -1,5 +1,8 @@
 package util;
 
+import Board.Tile;
+import Pieces.Piece;
+
 public class BoardTools {
 
     public static final boolean[] FIRST_COLUMN = initColumn(0);
@@ -7,18 +10,21 @@ public class BoardTools {
     public static final boolean[] SEVENTH_COLUMN = initColumn(7);
     public static final boolean[] EIGTH_COLUMN = initColumn(8);
 
+    public static final boolean[] SECOND_ROW = initRow(1);
+    public static final boolean[] SEVENTH_ROW = initRow(6);
+
     public static final int NUM_TILES = 64;
     public static final int ROW_LENGTH = 8;
+
+    public static boolean isValid(int coordinate) {
+        return coordinate >= 0 && coordinate < NUM_TILES;
+    }
 
 
     private static boolean[] initColumn(int N) {
 
         final boolean[] grid = new boolean[64];
 
-        // while (N < 64) {
-        //     grid[N] = true;
-        //     N += 8;
-        // }
         do {
             grid[N] = true;
             N += ROW_LENGTH;
@@ -26,5 +32,29 @@ public class BoardTools {
 
         return grid;
     }
+
+    private static boolean[] initRow(int N) {
+        int TileID = N * 8;
+
+        final boolean[] grid = new boolean[64];
+
+        do {
+            grid[TileID] = true;
+            TileID++;
+        } while (TileID % ROW_LENGTH != 0);
+
+        return grid;
+    }
+
+
+    public static String printTile(Tile tile) {
+        if (tile.isOccupied()) {
+            Piece piece = tile.getPiece();
+            return (piece.getColour() == Colour.WHITE) ? tile.toString().toLowerCase() :
+                    tile.toString();
+        }
+        return tile.toString();
+    }
+
     
 }
