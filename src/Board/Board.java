ef3a0fe2d7870e9e9a2e5c1d0c7000b34b0946f5
e63062kp <konrad.painta@student.manchester.ac.uk>
2023-12-03 17:54:47 +0000

    Build and populate board

--- src/Board/Board.java
+++ src/Board/Board.java
@@ -1,24 +1,93 @@
 package Board;
 
-import Pieces.Piece;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import util.BoardTools;
+import util.Colour;
+
+import Pieces.*;
 
 public class Board {
-    //Board array made up of pieces
-    private Piece[][] board;
 
-    public Board() {
-        setUp();
+    private final List<Tile> tileBoard;
+    
+    private Board(Builder builder) {
+        this.tileBoard = tileBoard(builder);
     }
-    private void setUp() {
-        board = new Piece[8][8];
 
-        for (int row=0; row<8; row++) {
-            for (int col=0; col<8; col++) {
-                board[row][col] = null;
-            }
+    /**
+     * Create and populate list of tiles. 
+     * @param builder
+     * @return
+     */
+    private List<Tile> tileBoard(Builder builder) {
+        final List<Tile> tiles = new ArrayList<>(BoardTools.NUM_TILES);
+        for (int i = 0; i < BoardTools.NUM_TILES; i++) {
+            tiles.add(new Tile(i, builder.boardConfig.get(i)));
+        }
+        return Collections.unmodifiableList(tiles);
+    }
+
+    /**
+     * Create and fill a board with standard piece positioning
+     * @return initialised and placed board
+     */
+    public static Board create() {
+        final Builder builder = new Builder();
+
+        //set black
+        builder.set(new Rook(0, Colour.BLACK));
+        builder.set(new Knight(1, Colour.BLACK));
+        builder.set(new Bishop(2, Colour.BLACK));
+        builder.set(new Queen(3, Colour.BLACK));
+        builder.set(new King(4, Colour.BLACK));
+        builder.set(new Bishop(5, Colour.BLACK));
+        builder.set(new Knight(6, Colour.BLACK));
+        builder.set(new Rook(7, Colour.BLACK));
+        for (int i=8; i<16; i++) { 
+            builder.set(new Pawn(i, Colour.BLACK));
+        }
+        //set white
+        builder.set(new Rook(56, Colour.WHITE));
+        builder.set(new Knight(57, Colour.WHITE));
+        builder.set(new Bishop(58, Colour.WHITE));
+        builder.set(new Queen(59, Colour.WHITE));
+        builder.set(new King(60, Colour.WHITE));
+        builder.set(new Bishop(61, Colour.WHITE));
+        builder.set(new Knight(62, Colour.WHITE));
+        builder.set(new Rook(63, Colour.WHITE));
+        for (int i=48; i<56; i++) { 
+            builder.set(new Pawn(i, Colour.WHITE));
+        }
+        //initialise player
+        builder.setCurrentPlayer(Colour.WHITE);
+
+        return builder.build();
+    }
+
+    public Tile getTile(int coordinate) { return null;}
+
+    public static class Builder {
+        Map<Integer, Piece> boardConfig;
+        Colour currentPlayer;
+
+        public Builder() {}
+
+        public Builder set(final Piece piece) {
+            boardConfig.put(piece.getCoordinates(), piece);
+            return this;
+        }
+        public Builder setCurrentPlayer(final Colour colour) {
+            this.currentPlayer = colour;
+            return this;
+        }
+        public Board build() {
+            return new Board(this);
         }
     }
-    public Tile getTile(int coordinate) { return null;}
 }
 
 
