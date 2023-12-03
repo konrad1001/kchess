fff23b56c74ba6f84244f982ee583540a9d2c7ee
e63062kp <konrad.painta@student.manchester.ac.uk>
2023-12-03 20:49:12 +0000

    Driver function

---
+++ src/kchess.java
@@ -0,0 +1,11 @@
+import Board.Board;
+
+public class kchess {
+    
+    public static void main(String[] args) {
+        Board board = Board.create();
+
+        System.out.println(board);
+    }
+
+}
