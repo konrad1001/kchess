5cfe4155b0c886468b29f38095dc624366a34d19
e63062kp <konrad.painta@student.manchester.ac.uk>
2023-12-06 17:18:51 +0000

    New Colour utilities

--- src/util/Colour.java
+++ src/util/Colour.java
@@ -1,10 +1,22 @@
 package util;
 
+import Player.Player;
+
 public enum Colour {
     WHITE {
         @Override
         public int getDirection() {
             return -1;
+        }
+
+        @Override
+        public Player choosePlayer(Player white, Player black) {
+            return white;
+        }
+
+        @Override
+        public Colour Opposite() {
+            return BLACK;
         }
     },
     BLACK {
@@ -12,7 +24,20 @@ public enum Colour {
         public int getDirection() {
             return 1;
         }
+
+        @Override
+        public Player choosePlayer(Player white, Player black) {
+            return black;
+        }
+
+        @Override
+        public Colour Opposite() {
+            return WHITE;
+        }
     };
 
     public abstract int getDirection();
-}
+    public abstract Player choosePlayer(Player white, Player black);
+    public abstract Colour Opposite();
+    }
+
