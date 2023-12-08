56d6ca54413100c6a38c1fcbe0b82c843b9e4233
e63062kp <konrad.painta@student.manchester.ac.uk>
2023-12-08 16:42:30 +0000

    MoveTypes are now used

--- src/Board/Move.java
+++ src/Board/Move.java
@@ -1,5 +1,7 @@
 package Board;
 
+import Board.Board.Builder;
+import Pieces.Pawn;
 import Pieces.Piece;
 import util.Colour;
 
@@ -8,33 +10,37 @@ public class Move {
     private final Board board;  //this board
     private final Piece movedPiece; //the piece to be moved
     private final int destination; //moving to this coordinate
-    private final boolean isCapturing; //are we capturing?
     private final Piece attackedPiece;
+    private final Colour colour; //what colour is making this move.
+    private final MoveType moveType;
 
     public Move() {
         this.board = null;
         this.movedPiece = null;
         this.attackedPiece = null;
         this.destination = 0;
-        this.isCapturing = false;
+        this.colour = null;
+        this.moveType = MoveType.NULL;
     }
 
     public Move(final Board board, final Piece movedPiece,
-        final int destination, final boolean isCapturing) {
+        final int destination, final MoveType moveType) {
             this.board = board;
             this.movedPiece = movedPiece;
             this.destination = destination;
-            this.isCapturing = isCapturing;
+            this.moveType = moveType;
             this.attackedPiece = null;
+            this.colour = movedPiece.getColour();
         }
     public Move(final Board board, final Piece movedPiece,
         final Piece attackedPiece,
-        final int destination, final boolean isCapturing) {
+        final int destination, final MoveType moveType) {
             this.board = board;
             this.movedPiece = movedPiece;
             this.attackedPiece = attackedPiece;
             this.destination = destination;
-            this.isCapturing = isCapturing;
+            this.colour = movedPiece.getColour();
+            this.moveType = moveType;
         }
     
         @Override
@@ -46,12 +52,113 @@ public class Move {
                      String.valueOf(position) + " to " + String.valueOf(destination);
         }
 
+        @Override
+        public int hashCode() {
+            final int prime = 31;
+            int result = 1;
+
+            result = prime * result + this.destination;
+            result = prime * result + this.movedPiece.hashCode();
+            if (moveType == MoveType.ATTACK) {
+                result = result + attackedPiece.hashCode();
+            }
+            return result;
+        }
+
+        @Override
+        public boolean equals(final Object other) {
+            if (this == other) {
+                return true;
+            }
+            if (!(other instanceof Move)) {
+                return false;
+            }
+            final Move otherMove = (Move) other;
+            return getCurrentCoordinate() == otherMove.getCurrentCoordinate() &&
+                    getDestinationCoordinates() == otherMove.getDestinationCoordinates() &&
+                    getMovedPiece().equals(getMovedPiece()) && getType() == otherMove.getType();
+
+        }
+
         public int getDestinationCoordinates() {
             return destination;
         }
+        public int getCurrentCoordinate() {
+            return movedPiece.getCoordinates();
+        }
+        public Colour getMoveColour() {
+            return colour;
+        }
+
+        public MoveType getType() {
+            return moveType;
+        }
+        public Piece getAttackedPiece() {
+            return attackedPiece;
+        }
+        public Piece getMovedPiece() {
+            return movedPiece;
+        }
 
         public Board execute() {
-            return null;
+            //executing a move returns a new board
+            final Builder builder = new Builder();
+            switch (moveType) {
+                case STANDARD:
+                    for (final Piece piece : board.getPieces(colour)) {
+                        if(!this.movedPiece.equals(piece)) {
+                            builder.set(piece);
+                        }
+                    }
+                    //set all of their unmoved.
+                    for (final Piece piece : board.getPieces(colour.Opposite())) {                       
+                        builder.set(piece);
+                    }
+                    builder.set(movedPiece.movePiece(this));
+                    break;
+                case ATTACK:
+                    for (final Piece piece : board.getPieces(colour)) {
+                        if(!this.movedPiece.equals(piece)) {
+                            builder.set(piece);
+                        }
+                    }
+                    //set all of their unmoved unless it is attacked.
+                    for (final Piece piece : board.getPieces(colour.Opposite())) {
+                        if(!this.attackedPiece.equals(piece)) {
+                            builder.set(piece);
+                        }
+                    }
+                    builder.set(movedPiece.movePiece(this));
+                    break;
+                    
+                case PAWN_ATTACK:
+                    break;
+                case PAWN_ENPASSANT_ATTACK:
+                    break;
+                case PAWN_JUMP:
+                    for (final Piece piece : board.getPieces(colour)) {
+                        if(!this.movedPiece.equals(piece)) {
+                            builder.set(piece);
+                        }
+                    }
+                    for (final Piece piece : board.getPieces(colour.Opposite())) {                       
+                        builder.set(piece);
+                    }
+                    final Pawn pawn = (Pawn) this.movedPiece.movePiece(this);
+                    builder.set(pawn);
+                    builder.setEnPassantPawn(pawn);
+                    break;
+                case PAWN_MOVE:
+                    break;
+                default:
+                    break;
+                
+            }
+            
+            
+            //switch teams
+            builder.setCurrentPlayer(colour.Opposite());
+            return builder.build();
         }
 
 }
