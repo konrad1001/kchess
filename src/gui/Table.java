ec68a9623bd78455dd1bfbef66de720d137b42db
e63062kp <konrad.painta@student.manchester.ac.uk>
2023-12-08 17:53:12 +0000

    Fleshing out the GUI with Panels

--- src/gui/Table.java
+++ src/gui/Table.java
@@ -1,24 +1,89 @@
 package gui;
 
 import javax.swing.*;
+
+import util.BoardTools;
+
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Table {
     
     private final JFrame gameFrame;
 
+    private final BoardPanel boardPanel;
+
     private static final Dimension FRAME_DIMENSIONS = new Dimension(600,600);
 
     public Table() {
         this.gameFrame = new JFrame("kChess");
+        this.gameFrame.setLayout(new BorderLayout());
 
-        
+        final JMenuBar tableMenuBar = createMenuBar();
+
+        this.gameFrame.setJMenuBar(tableMenuBar);
         this.gameFrame.setSize(FRAME_DIMENSIONS);
+
+        this.boardPanel = new BoardPanel();
+        this.gameFrame.add(this.boardPanel, BorderLayout.CENTER);
+
         this.gameFrame.setVisible(true);
     }
 
-    
+    private JMenuBar createMenuBar() {
+        final JMenuBar tableMenuBar = new JMenuBar();
+        tableMenuBar.add(createFileMenu());
+        return tableMenuBar;
+    }
+
+    private JMenu createFileMenu() {
+        final JMenu fileMenu = new JMenu("File");
+        final JMenuItem openPGN = new JMenuItem("Load PGN File");
+        openPGN.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("open pgn");
+            }
+        });
+        fileMenu.add(openPGN);
+        return fileMenu;
+        
+    }
+
+    private class BoardPanel extends JPanel {
+        private static final Dimension BOARD_PANEL_DIMENSIONS = new Dimension(400, 350);
+        final List<TilePanel> boardTiles; 
+
+        BoardPanel() {
+            super(new GridLayout(8, 8));
+            this.boardTiles = new ArrayList<>();
+            for(int i=0; i<BoardTools.NUM_TILES; i++) {
+                final TilePanel tilePanel = new TilePanel(this, i);
+                boardTiles.add(tilePanel);
+                add(tilePanel);
+            }
+            setPreferredSize(BOARD_PANEL_DIMENSIONS);
+            validate();
+        }
+    }
+
+    private class TilePanel extends JPanel {
+        private static final Dimension TILE_PANEL_DIMENSION = new Dimension(10, 10);
+        private final int tileID;
+
+        TilePanel(final BoardPanel boardPanel, final int tileID) {
+            super(new GridBagLayout());
+            this.tileID = tileID;
+            setPreferredSize(TILE_PANEL_DIMENSION);
+            assignTileColour();
+            validate();
+        }
+
+        private void assignTileColour() {
+        }
+    }
 
 }
