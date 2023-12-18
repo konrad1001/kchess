eb720d04b6e0ab7110c32e115923e7a2aafe77e2
e63062kp <konrad.painta@student.manchester.ac.uk>
2023-12-18 14:55:08 +0000

    Taken Pieces panel further implementation

--- src/gui/TakenPiecesGUI.java
+++ src/gui/TakenPiecesGUI.java
@@ -1,11 +1,16 @@
 package gui;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -15,34 +20,40 @@ import Pieces.Piece;
 import gui.ChessBoardGUI.Log;
 import util.BoardTools;
 import util.Colour;
-
-import static gui.ChessBoardGUI.*;
+;
 
 public class TakenPiecesGUI extends JPanel {
 
     private final JPanel whiteTakenPanel;
     private final JPanel blackTakenPanel;
 
-    private static final Dimension PANEL_DIMENSION = new Dimension(40, 80);
+    private static final Dimension PANEL_DIMENSION = new Dimension(60, 50);
 
     public TakenPiecesGUI() {
         super(new BorderLayout());
-        setBackground(java.awt.Color.decode("#FFFACD"));
+        setBackground(Color.decode("#BDCFEA")); //light tile colour
+        setPreferredSize(PANEL_DIMENSION);
         
-        this.whiteTakenPanel = new JPanel(new GridLayout(8, 2));
-        this.blackTakenPanel = new JPanel(new GridLayout(8, 2));
+        this.whiteTakenPanel = new JPanel(new GridLayout(1, 8));
+        this.blackTakenPanel = new JPanel(new GridLayout(1, 8));
+
+        this.whiteTakenPanel.setBackground(Color.decode("#BDCFEA")); 
+        this.blackTakenPanel.setBackground(Color.decode("#BDCFEA")); 
+
+        this.add(whiteTakenPanel, BorderLayout.NORTH);
+        this.add(blackTakenPanel, BorderLayout.SOUTH);
     }
 
     public void redraw(final Log moveLog) {
         whiteTakenPanel.removeAll();
         blackTakenPanel.removeAll();
-
         final List<Piece> whiteTakenPieces = new ArrayList<>();
         final List<Piece> blackTakenPieces = new ArrayList<>();
 
         for (final Move move : moveLog.getMoves()) {
             if (move.isAttack()) {
                 final Piece takenPiece = move.getInteractedPiece();
+                
                 if (takenPiece.getColour() == Colour.WHITE) {
                     whiteTakenPieces.add(takenPiece);
                 } else if (takenPiece.getColour() == Colour.BLACK) {
@@ -58,8 +69,11 @@ public class TakenPiecesGUI extends JPanel {
         for (final Piece takenPiece : whiteTakenPieces) {
             try {
                 final String path = BoardTools.getPiecePath(takenPiece);
-                final ImageIcon icon = new ImageIcon(path);
-                final JLabel imageLabel = new JLabel(icon);
+                
+                final BufferedImage image = ImageIO.read(new File(path));
+                Image scaledImage = image.getScaledInstance(25, 25, Image.SCALE_SMOOTH);
+                final JLabel imageLabel = new JLabel(new ImageIcon(scaledImage));
+                
                 this.whiteTakenPanel.add(imageLabel);
             } catch (Exception e) {
                 System.out.printf("Error loading piece image for taken %s", takenPiece);
@@ -68,9 +82,10 @@ public class TakenPiecesGUI extends JPanel {
         for (final Piece takenPiece : blackTakenPieces) {
             try {
                 final String path = BoardTools.getPiecePath(takenPiece);
-                final ImageIcon icon = new ImageIcon(path);
-                final JLabel imageLabel = new JLabel(icon);
-                this.whiteTakenPanel.add(imageLabel);
+                final BufferedImage image = ImageIO.read(new File(path));
+                Image scaledImage = image.getScaledInstance(25, 25, Image.SCALE_SMOOTH);
+                final JLabel imageLabel = new JLabel(new ImageIcon(scaledImage));
+                this.blackTakenPanel.add(imageLabel);
             } catch (Exception e) {
                 System.out.printf("Error loading piece image for taken %s", takenPiece);
             }
