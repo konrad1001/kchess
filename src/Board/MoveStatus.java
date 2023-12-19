27d1928c72ad0a2e404c594b02109b33a8b60980
e63062kp <konrad.painta@student.manchester.ac.uk>
2023-12-19 13:53:40 +0000

    Cleaner MoveStatus

--- src/Board/MoveStatus.java
+++ src/Board/MoveStatus.java
@@ -1,25 +1,8 @@
 package Board;
 
 public enum MoveStatus {
-    ILLEGAL_MOVE {
-        @Override
-        public boolean isDone() {
-            return false;
-        }
-    },
-    DONE {
-        @Override
-        public boolean isDone() {
-            return false;
-        }
-    }, LEAVES_PLAYER_IN_CHECK {
-        @Override
-        public boolean isDone() {
-            // TODO Auto-generated method stub
-            throw new UnsupportedOperationException("Unimplemented method 'isDone'");
-        }
-    };
-
-    public abstract boolean isDone();
+    ILLEGAL_MOVE,
+    DONE,
+    LEAVES_PLAYER_IN_CHECK
 
 }
